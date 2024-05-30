24d7878c6ddc9e758fae9deda0fa7676944fcca0
Leesa Ward <leesa@doubleedesign.com.au>
2024-05-30 13:36:05 +1000

    CI: Add unit test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,20 @@ export default {
 	rootDir: './',
 	automock: false, // requires jest-fetch-mock
 	setupFilesAfterEnv: ['./jest.setup.ts'],
+	coverageThreshold: {
+		'./src/datasources': {
+			branches: 75,
+			functions: 90,
+			lines: 90,
+			statements: 90,
+		},
+		'./src/datawranglers': {
+			branches: 75,
+			functions: 90,
+			lines: 90,
+			statements: 90,
+		},
+	},
 	transform: {
 		'^.+\\.(ts|tsx)?$': ['babel-jest', {
 			presets: [
